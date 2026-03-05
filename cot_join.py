c4b96aa1b2a21b391461add37caf0f809f63a68c
yanan1116 <yananchen1116@gmail.com>
2026-03-05 18:32:41 +0000

    ablation for rm text;

--- cot_join.py
+++ cot_join.py
@@ -16,6 +16,7 @@ import argparse
 import hashlib
 import json
 import os
+import pickle
 import re
 import shutil
 from dataclasses import dataclass
@@ -130,7 +131,6 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--libero_repo", default="physical-intelligence/libero")
     parser.add_argument("--molmo_repo", default="yananchen/molmoact_libero_cot")
-
     parser.add_argument("--instruction_col", default="instruction")
     parser.add_argument("--libero_image_col", default="image")
     parser.add_argument("--libero_wrist_col", default="wrist_image")
@@ -145,68 +145,82 @@ def main() -> None:
         path=args.libero_repo,
         split='train',
     )
-
+    # ds_libero = ds_libero.shuffle().select(range(10000))
     ds_libero = ds_libero.map(add_instruction, num_proc=16, desc='add instruction')
-
-
-    ds_molmo = load_dataset(
-        path=args.molmo_repo,
-        split='train',
-    )
-    print(f"ds_libero: {len(ds_libero)} rows")
-    print(f"ds_molmoact: {len(ds_molmo)} rows")
-
     ds_libero = ensure_decode_false(ds_libero, args.libero_image_col)
     ds_libero = ensure_decode_false(ds_libero, args.libero_wrist_col)
-    ds_molmo = ensure_decode_false(ds_molmo, args.molmo_image_col)
-    ds_molmo = ensure_decode_false(ds_molmo, args.molmo_wrist_col)
 
-    print("Building exact index from ds_molmoact...")
-    molmo_index, ambiguous_keys_cnt = build_molmo_index(
-        ds_molmo,
-        instruction_col=args.instruction_col,
-        image_col=args.molmo_image_col,
-        wrist_col=args.molmo_wrist_col,
-        depth_col=args.depth_col,
-        eef_traj_col=args.eef_traj_col,
-    )
+    ds_libero_task_index = set(list(ds_libero['task_index']))
 
-    unique_keys = sum(v is not None for v in molmo_index.values())
-    print(f"molmo_index keys: {len(molmo_index)}")
-    print(f"molmo_unique_keys: {unique_keys}")
-    print(f"molmo_ambiguous_keys: {ambiguous_keys_cnt}")
-    assert ambiguous_keys_cnt == 0
+    print(f"ds_libero: {len(ds_libero)} rows  distinct task index: {len(ds_libero_task_index)} ")
+
+    if not Path("./molmo_index.pkl").is_file():
+        ds_molmo = load_dataset(
+            path=args.molmo_repo,
+            split='train',
+        )
+        ds_molmo = ensure_decode_false(ds_molmo, args.molmo_image_col)
+        ds_molmo = ensure_decode_false(ds_molmo, args.molmo_wrist_col)
+        print(f"ds_molmoact: {len(ds_molmo)} rows")
+        print("Building exact index from ds_molmoact...")
+        molmo_index, ambiguous_keys_cnt = build_molmo_index(
+            ds_molmo,
+            instruction_col=args.instruction_col,
+            image_col=args.molmo_image_col,
+            wrist_col=args.molmo_wrist_col,
+            depth_col=args.depth_col,
+            eef_traj_col=args.eef_traj_col,
+        )
+        unique_keys = sum(v is not None for v in molmo_index.values())
+        print(f"molmo_index keys: {len(molmo_index)}")
+        print(f"molmo_unique_keys: {unique_keys}")
+        print(f"molmo_ambiguous_keys: {ambiguous_keys_cnt}")
+        assert ambiguous_keys_cnt == 0
+        with open('./molmo_index.pkl', "wb") as f:
+            pickle.dump(molmo_index, f, protocol=pickle.HIGHEST_PROTOCOL, )
+    else:
+        with open("./molmo_index.pkl", "rb") as f:
+            molmo_index = pickle.load(f)
 
     print("Joining onto ds_libero...")
-    matched = 0
     unmatched = 0
-    ambiguous_hit = 0
     joined_depth = []
     joined_eef = []
 
+    missed_task_index = set()
     for ex in tqdm(ds_libero, total=len(ds_libero), desc="Joining ds_libero"):
 
         key = make_key(ex[args.instruction_col], ex[args.libero_image_col], ex[args.libero_wrist_col])
-        payload = molmo_index.get(key, "MISS")
-        if payload == "MISS":
+        payload = molmo_index.get(key, None)
+        if not payload:
             unmatched += 1
+            missed_task_index.add(ex['task_index'])
+            joined_depth.append('')
+            joined_eef.append('')            
         else:
-            assert payload
-            matched += 1
             joined_depth.append(payload.depth)
             joined_eef.append(payload.eef_traj)
 
-    hit_rate = matched / len(ds_libero)
-    miss_rate = unmatched / len(ds_libero)
+    print(f"unmatched: {unmatched}", unmatched / len(ds_libero))
+    print( "missed_task_index:", len(missed_task_index), len(missed_task_index) /  len(ds_libero_task_index) )
 
-    print(f"total_libero: {total}")
-    print(f"matched: {matched}")
-    print(f"unmatched: {unmatched}")
-    print(f"hit_rate: {hit_rate:.6f}")
-    print(f"miss_rate: {miss_rate:.6f}")
+    assert len(joined_depth) == len(ds_libero)
+    assert len(joined_eef) == len(ds_libero)
 
+    ds_libero = ds_libero.add_column("depth", joined_depth)
+    ds_libero = ds_libero.add_column("eef_traj", joined_eef)
+    # ds_libero_filter = ds_libero.filter(lambda ex: ex['task_index'] not in missed_task_index)
+    ds_libero_filter = ds_libero.filter(lambda ex: ex['depth'] != '' and ex['eef_traj'] != '')
 
+    print('ds_libero_filter:', ds_libero_filter.num_rows / ds_libero.num_rows)
 
+    ds_libero_sample = ds_libero_filter.select(range(10000))
+
+    ds_libero_filter.push_to_hub(
+        f"yananchen/libero_cot",
+        private=False,
+        embed_external_files=True,
+    )
 
 if __name__ == "__main__":
     main()
